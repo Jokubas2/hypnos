53a21b43dbe56c84325c437dab940dc18d445c3d
Jokubas <jokjas47@gmail.com>
2021-09-09 19:31:29 +0300

    Add empty project

---
+++ hypnos/Assets/Scripts/Jump.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Jump : MonoBehaviour
+{
+    public float fallMarkiplier = 2.5f;
+    public float lowJumpMarkiplier = 2f;
+
+    Rigidbody rb;
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+
+    void Update()
+    {
+        if(rb.velocity.y<0)
+        {
+            rb.velocity += Vector3.up * Physics.gravity.y * (fallMarkiplier - 1) * Time.deltaTime;
+        }
+        else if(rb.velocity.y>0 && !Input.GetKey(KeyCode.Space))
+        {
+            rb.velocity += Vector3.up * Physics.gravity.y * (lowJumpMarkiplier - 1) * Time.deltaTime;
+        }
+    }
+
+}
---
+++ hypnos/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,111 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public Vector3 jump;
+
+    public float jumpForgive = 0.5f;
+    public float jumpTime = 0.5f;
+
+    float jumpForgiveness;
+    float jumpLength;
+
+    public float jumpForce = 4.0f;
+    public float movementSpeed = 2.0f;
+
+    public bool isGrounded;
+
+
+
+    GameObject lastCollider;
+    GameObject currentCollider;
+
+    int jumpDelay = 0;
+    int jumpTimer = 0;
+    
+
+    Rigidbody rb;
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        jump = new Vector3(0.0f, 2.0f, 0.0f);
+
+        jumpForgiveness = jumpForgive;
+        jumpLength = jumpTime;
+        jumpForgiveness *= 50;
+        jumpLength *= 50;
+
+        Physics.gravity = new Vector3(0, -9.83f, 0);
+    }
+
+    void OnCollisionStay(Collision collision)
+    {
+        isGrounded = true;
+        currentCollider = collision.gameObject;
+    }
+    void OnCollisionExit()
+{
+        isGrounded = false;
+    }
+
+
+    int x = 1, y = 0, z = 0;
+
+    private void FixedUpdate()
+    {
+        if(jumpDelay>=0)
+        jumpDelay--;
+        if(jumpTimer>=0)
+        jumpTimer--;
+
+    }
+
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            jumpDelay = (int)jumpForgiveness;
+        }
+
+        if (jumpDelay > 0 && isGrounded )
+        {
+            if((currentCollider==lastCollider && jumpTimer < 0)||currentCollider!=lastCollider)
+            {
+                lastCollider = currentCollider;
+                jumpDelay = -1;
+                jumpTimer = (int)jumpLength;
+                rb.AddForce(jump * jumpForce, ForceMode.Impulse);
+                isGrounded = false;
+            }
+            
+        }
+
+
+        if (Input.GetKey(KeyCode.D))
+        {
+            Vector3 direction = new Vector3(1.0f, 0.0f, 0.0f);
+            rb.AddForce(direction * movementSpeed, ForceMode.Acceleration);
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            Vector3 direction = new Vector3(-1.0f, 0.0f, 0.0f);
+            rb.AddForce(direction * movementSpeed, ForceMode.Acceleration);
+        }
+        if (Input.GetKey(KeyCode.W))
+        {
+            Vector3 direction = new Vector3(0.0f, 0.0f, 1.0f);
+            rb.AddForce(direction * movementSpeed, ForceMode.Acceleration);
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            Vector3 direction = new Vector3(0.0f, 0.0f, -1.0f);
+            rb.AddForce(direction * movementSpeed, ForceMode.Acceleration);
+        }
+    }
+
+
+
+    
+}
