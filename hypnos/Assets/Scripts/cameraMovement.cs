ad9207f87bd957436ea5286a4074e9054607511a
Jokubas <jokjas47@gmail.com>
2021-10-14 21:13:35 +0300

    Player movement w/rb

---
+++ hypnos/Assets/Scripts/cameraMovement.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cameraMovement : MonoBehaviour
+{
+
+
+    public Transform player;
+
+    void Update()
+    {
+        transform.position = player.transform.position;
+    }
+}
